649c2f26d6f01149b5de9600d6454577628c3304
@ivelton2018 <ivelton2018@gmail.com>
2018-10-23 10:27:56 -0400

    material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import Paper from '@material-ui/core/Paper';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Checkbox from '@material-ui/core/Checkbox';
+import IconButton from '@material-ui/core/IconButton';
+import CommentIcon from '@material-ui/icons/Comment';
 
 const todosQuery = gql`
 {
@@ -14,6 +22,26 @@ const todosQuery = gql`
 
 
 class App extends Component {
+  state = {
+    checked: [0],
+  };
+
+  handleToggle = value => () => {
+    const { checked } = this.state;
+    const currentIndex = checked.indexOf(value);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(value);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    this.setState({
+      checked: newChecked,
+    });
+  };
+
   render() {
     const {data: {loading, todos}} = this.props;
     console.log("todos:", todos)
@@ -21,8 +49,35 @@ class App extends Component {
       return null;
     }
     return (
-      <div>
-        {todos.map(todo => <div key={todo.id}>{todo.id} - {todo.text}</div>)} 
+      <div style={{display:"flex"}}>
+        <div style={{margin:"auto", width: 400 }}>
+          <Paper elevation={1}>
+            <List>
+              {todos.map(todo => (
+                <ListItem
+                  key={todo.id}
+                  role={undefined}
+                  dense
+                  button
+                  onClick={this.handleToggle(todo)}
+                >
+                  <Checkbox
+                    checked={todo.complete}
+                    tabIndex={-1}
+                    disableRipple
+                  />
+                  <ListItemText primary={todo.text} />
+                  <ListItemSecondaryAction>
+                    <IconButton aria-label="Comments">
+                      <CommentIcon />
+                    </IconButton>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
+          
+          </Paper>
+        </div>   
       </div>
     );
   }
